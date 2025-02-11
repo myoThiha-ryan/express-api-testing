59235595202bf990e12feb80b104e433feac3654
Myo Thiha <mth2682004@gmail.com>
2025-02-11 21:36:13 +0000

    created middleware

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import express from "express";
 
 const app = express();
-app.use(express.json());
-
 const PORT = process.env.PORT || 4000;
 
 const users = [
@@ -14,7 +12,29 @@ const users = [
   { id: 6, name: "Simon", age: 21 },
 ];
 
+const loggingMiddleware = (req, res, next) => {
+  console.log(req.method + " - " + req.url);
+  next();
+};
+
+const resolveIndexByUserId = (req, res, next) => {
+  const {
+    params: { id },
+  } = req;
+  const parsedId = parseInt(id);
+  if (isNaN(parsedId)) {
+    return res.sendStatus(404);
+  }
+  const userIndex = users.findIndex((user) => user.id === parsedId);
+  if (userIndex === -1) {
+    return res.sendStatus(404);
+  }
+  req.userIndex = userIndex;
+  next();
+};
+
 app.use(express.json());
+app.use(loggingMiddleware);
 
 app.get("/", (req, res) => {
   res.send("API is working");
@@ -34,17 +54,9 @@ app.get("/api/user", (req, res) => {
 
 // Route Params
 
-app.get("/api/user/:id", (req, res) => {
-  const { id } = req.params;
-  const parsedId = parseInt(id);
-  console.log(parsedId);
-  if (isNaN(parsedId)) {
-    return res.status(501).send({ msg: "requested id not valid" });
-  }
-  const requestedUser = users.find((user) => user.id === parsedId);
-  if (!requestedUser) {
-    res.status(404).send({ msg: "there is no user with this Id" });
-  }
+app.get("/api/user/:id", resolveIndexByUserId, (req, res) => {
+  const { userIndex } = req;
+  const requestedUser = users[userIndex];
   res.send(requestedUser);
 });
 
@@ -59,56 +71,24 @@ app.post("/api/user", (req, res) => {
 
 // PUT request
 
-app.put("/api/user/:id", (req, res) => {
-  const {
-    body,
-    params: { id },
-  } = req;
-  const parsedId = parseInt(id);
-  if (isNaN(parsedId)) {
-    return res.sendStatus(404);
-  }
-  const userIndex = users.findIndex((user) => user.id === parsedId);
-  if (userIndex === -1) {
-    return res.sendStatus(404);
-  }
-  users[userIndex] = { id: parsedId, ...body };
+app.put("/api/user/:id", resolveIndexByUserId, (req, res) => {
+  const { userIndex, body } = req;
+  users[userIndex] = { id: users[userIndex].id, ...body };
   res.sendStatus(204);
 });
 
 // PATCH request
 
-app.patch("/api/user/:id", (req, res) => {
-  const {
-    body,
-    params: { id },
-  } = req;
-  const parsedId = parseInt(id);
-  if (isNaN(parsedId)) {
-    return res.sendStatus(404);
-  }
-  const userIndex = users.findIndex((user) => user.id === parsedId);
-  if (userIndex === -1) {
-    return res.sendStatus(404);
-  }
+app.patch("/api/user/:id", resolveIndexByUserId, (req, res) => {
+  const { userIndex, body } = req;
   users[userIndex] = { ...users[userIndex], ...body }; // way of updating with PATCH request
   res.sendStatus(204);
 });
 
 // DElETE request
 
-app.delete("/api/user/:id", (req, res) => {
-  const {
-    params: { id },
-  } = req;
-  const parsedId = parseInt(id);
-  if (isNaN(parsedId)) {
-    return res.sendStatus(400);
-  }
-  const userIndex = users.findIndex((user) => user.id === parsedId);
-  if (userIndex === -1) {
-    return res.sendStatus(404);
-  }
+app.delete("/api/user/:id", resolveIndexByUserId, (req, res) => {
+  const { userIndex } = req;
   users.splice(userIndex, 1);
   res.sendStatus(200);
 });
