bfadadfd3ee4a3bbdbd4a3cc0a0ecb9dba55f893
Myo Thiha <mth2682004@gmail.com>
2025-02-11 00:58:57 +0000

    created query params

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 4000;
 const users = [
   { id: 1, name: "Myo", age: 21 },
   { id: 2, name: "Nanm", age: 21 },
+  { id: 3, name: "James", age: 21 },
+  { id: 4, name: "Sarah", age: 21 },
+  { id: 5, name: "Tony", age: 21 },
+  { id: 6, name: "Simon", age: 21 },
 ];
 
 app.use(express.json());
@@ -14,13 +18,21 @@ app.get("/", (req, res) => {
   res.send("API is working");
 });
 
+// Query Params
 app.get("/user", (req, res) => {
+  const {
+    query: { filter, name },
+  } = req;
+  if (filter && name) {
+    const filteredUsers = users.filter((user) => user[filter].includes(name));
+    res.send(filteredUsers);
+  }
   res.send(users);
 });
 
 // Route Params
 
-app.get("/users/:id", (req, res) => {
+app.get("/user/:id", (req, res) => {
   const { id } = req.params;
   console.log(id);
   const parsedId = parseInt(id);
